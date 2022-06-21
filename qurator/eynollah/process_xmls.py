6f9363a32bc65adba33bdc184e1ec30d4d317e7b
Hasnain <hasnain-ali@bitbucket.org>
2022-06-21 18:09:42 +0500

    fix

--- qurator/eynollah/process_xmls.py
+++ qurator/eynollah/process_xmls.py
@@ -6,7 +6,7 @@ import os
 import sys
 from collections import defaultdict
 import xml.etree.ElementTree as ET
-from xml.etree.ElementTree importr ParseError
+from xml.etree.ElementTree import ParseError
 
 FINAL_OUTPUT_DIR = os.path.dirname(os.getcwd()) + "/output/data/international_data/original_uploaded_files/ar"
 
